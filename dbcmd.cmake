aac69681d737b3829da9a645a01db6a8dc999799
Adrian Vasile Constantin <adrian.vasile.constantin@gmail.com>
2024-11-19 21:38:43 +0200

    Add install commands to cmake files.
    Add install and export commands for all cmake targets: local boost, odbc++ libraray and dbcmd executable.
    
    Use CMake module GenerateExportHeader to generate ODBCXX_export.h, and remove the manually written Exports.h.

---
+++ dbcmd.cmake
@@ -0,0 +1,53 @@
+include(GNUInstallDirs)
+
+set(SQLODBC_SOURCES
+    "src/dbcmd/external/boost/iostreams.cc"
+    "src/dbcmd/Context.cc"
+    "src/dbcmd/CommandHandler.cc"
+    "src/dbcmd/lib/HistoryList.cc"
+    "src/dbcmd/lib/HistoryFile.cc"
+    "src/dbcmd/lib/HistoryCache.cc"
+    "src/dbcmd/browseConnect.cc"
+    "src/dbcmd/Drivers.cc"
+    "src/dbcmd/ShellCommand.cc"
+    "src/dbcmd/ChDirCommand.cc"
+    "src/dbcmd/EnvCommand.cc"
+    "src/dbcmd/EchoCommand.cc"
+    "src/dbcmd/HelpCommand.cc"
+    "src/dbcmd/DiagnosticRecord.cc"
+    "src/sqlodbc.cc")
+
+add_executable(dbcmd)
+target_sources(dbcmd PRIVATE FILE_SET CXX_MODULES FILES ${SQLODBC_SOURCES})
+target_link_libraries(dbcmd PRIVATE odbc++) # boost_iostreams
+target_compile_features(dbcmd PRIVATE cxx_std_23)
+
+message(STATUS "CXX compiler import std: ${CMAKE_CXX_COMPILER_IMPORT_STD}")
+
+if("23" IN_LIST CMAKE_CXX_COMPILER_IMPORT_STD)
+    set_target_properties(dbcmd PROPERTIES CXX_MODULE_STD ON)
+else()
+    message(SEND_ERROR "C++ support for import std not implemented in the build system, see available compiler options")
+endif()
+    
+set_target_properties(dbcmd
+    PROPERTIES
+	MSVC_DEBUG_INFORMATION_FORMAT "EditAndContinue"
+	COMPILE_PDB_NAME "sqlodbc.compile"
+	CXX_SCAN_FOR_MODULES ON
+	INTERPROCEDURAL_OPTIMIZATION_RELEASE TRUE)
+
+if(WIN32)
+    find_library(SHELL32_LIBRARY_PATH "Shell32" REQUIRED)
+    target_compile_definitions(dbcmd PRIVATE WINDOWS)
+    target_link_libraries(dbcmd PRIVATE "${SHELL32_LIBRARY_PATH}")
+
+    if(MSVC)
+	target_include_directories(dbcmd PRIVATE "src/dbcmd")
+    endif()
+endif()
+
+install(TARGETS dbcmd EXPORT dbcmd
+    RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}" COMPONENT Runtime)
+
+install(EXPORT dbcmd DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake" COMPONENT Development)
---
+++ odbc++.cmake
@@ -0,0 +1,68 @@
+include(GNUInstallDirs)
+include(GenerateExportHeader)
+
+set(ODBCXX_HEADERS
+    "include/odbc++/SQLDiagnosticException.hh"
+    "include/odbc++/Handle.hh"
+    "include/odbc++/Environment.hh"
+    "include/odbc++/Connection.hh"
+    "include/odbc++/WindowsCategory.hh"
+    "include/odbc++/FileStreamBuffer.hh"
+    "include/odbc++/FileDescriptorDevice.hh"
+    "include/odbc++/FileDescriptorSink.hh"
+    "include/odbc++/FileDescriptorSource.hh"
+    "include/odbc++/FileDescriptor.hh"
+)
+
+set(ODBCXX_SOURCES
+    "src/odbc++/Handle.cc"
+    "src/odbc++/Environment.cc"
+    "src/odbc++/Connection.cc"
+    "src/odbc++/WindowsCategory.cc"
+    "src/odbc++/FileStreamBuffer.cc")
+
+add_library(odbc++ SHARED ${ODBCXX_SOURCES})
+target_sources(odbc++ PUBLIC FILE_SET HEADERS TYPE HEADERS BASE_DIRS "include" FILES ${ODBCXX_HEADERS})
+target_include_directories(odbc++ PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>" "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>")
+set_target_properties(odbc++
+    PROPERTIES
+	MSVC_DEBUG_INFORMATION_FORMAT "EditAndContinue"
+	COMPILE_PDB_NAME "odbc++.compile"
+	CXX_SCAN_FOR_MODULES ON
+	INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+
+target_compile_features(odbc++ PUBLIC cxx_std_23)
+target_link_libraries(odbc++ PUBLIC local_boost::iostreams)
+
+generate_export_header(odbc++
+	BASE_NAME odbcxx
+	EXPORT_FILE_NAME "ODBCXX_Export.h"
+	INCLUDE_GUARD_NAME SQL_ODBCXX_EXPORT_H
+	EXPORT_MACRO_NAME ODBCXX_EXPORT
+	NO_EXPORT_MACRO_NAME MODULE_LOCAL)
+
+target_include_directories(odbc++ PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>")
+
+if("23" IN_LIST CMAKE_CXX_COMPILER_IMPORT_STD)
+    set_target_properties(odbc++ PROPERTIES CXX_MODULE_STD ON)
+else()
+    message(SEND_ERROR "C++ support for import std not implemented in the build system, see available compiler options")
+endif()
+
+if(WIN32)
+    find_library(ODBC_LIBRARY_PATH "ODBC32" REQUIRED)
+    target_link_libraries(odbc++ PRIVATE "${ODBC_LIBRARY_PATH}")
+    target_compile_definitions(odbc++ PRIVATE WINDOWS)
+endif()
+
+install(TARGETS odbc++ EXPORT odbc++
+    PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/odbc++" COMPONENT Development
+    FILE_SET HEADERS DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}" COMPONENT Development
+    ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}" COMPONENT Development
+    LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}" COMPONENT Runtime NAMELINK_COMPONENT Development
+    RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}" COMPONENT Runtime)
+
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/ODBCXX_Export.h"
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/odbc++" COMPONENT Development)
+
+install(EXPORT odbc++ COMPONENT Development DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake")
